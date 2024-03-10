6e34bc7742419ed1320e794ff06ce706eadc9596
winnie <winniejebiwott99@gmail.com>
2024-03-10 09:06:48 +0300

    updated some of the composer package

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'surname',
         'email',
         'password',
+        
     ];
 
     /**
